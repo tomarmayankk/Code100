7cce8161ce5d19a905d3dadf951942c1e0843f5e
tomarmayankk <mayanktomarpc@gmail.com>
2024-08-29 16:27:27 +0530

    Numbers

--- Numbers/Q9.java
+++ Numbers/Q9.java
@@ -2,13 +2,11 @@ package Numbers;
 //number of digits in an integer
 public class Q9 {
     public static void main(String[] args){
-        int integer = 1234568756;
+        int n = 123444567;
         int digit = 0;
-
-        while (integer!= 0) {
-            int n = integer % 10;
+        while (n!= 0) {
+            n = n/10;
             digit++;
-            integer = integer/10;
         }
         System.out.println(digit);
     }
